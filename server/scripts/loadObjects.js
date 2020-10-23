dfffbfc4258089579c098682a68e5601af04488b
Joeken, Fabian <Fabian.joeken@hsrw.org>
2020-10-23 13:53:44 +0200

    Typen in anderen Typen referenzieren

--- server/scripts/loadObjects.js
+++ server/scripts/loadObjects.js
@@ -6,8 +6,9 @@ var connection = db.createConnection({
     password: 'admin',
     database: 'dokutool'
 })
+
 // Import Types
-const types =  require("./types.json");
+const types = require("./types.json");
 
 //XMLParser und Fileread
 const xml2js = require('xml2js');
@@ -25,17 +26,36 @@ for (let i = 2; i < process.argv.length; i++) {
             var colums = [];
             result.object.section.forEach(element => {
                 element.input.forEach(input => {
-                    //label fuer die Datenbank zu LowerCase umwandeln 
-                    let attributeName = JSON.parse(JSON.stringify(input.label).toLowerCase());
+                    //name fuer Spalte in der Datenbank festlegen
+                    let attributeName;
+                    if (input['attr'].object) {
+                        //ueberpruefen, ob Tabelle und damit Typ mit dem angegebenen Objectname existiert
+                        connection.query("SHOW TABLES LIKE '" + JSON.parse(JSON.stringify(input['attr'].object).toLocaleLowerCase()) + "';", function (error, results) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                if (results.length === 0) {
+                                    console.log("Es existiert kein Typ mit dem Namen " + input['attr'].object + ". Bitte legen Sie zuerst einen Typen mit diesem Namen an bevor Sie ihn referenzieren.");
+                                    process.exit();
+                                }
+                            }
+                        });
+                        attributeName = input['attr'].object;
+                    } else {
+                        attributeName = input.label;
+                    }
+                    //label fuer die Datenbank zu LowerCase umwandeln
+                    attributeName = JSON.parse(JSON.stringify(attributeName).toLowerCase());
                     //leerzeichen durch _ ersetzen
                     if (JSON.stringify(attributeName).includes(" ")) {
                         attributeName = JSON.parse(JSON.stringify(attributeName).replace(" ", "_"));
                     }
                     var colum = attributeName + " ";
-                    if (types[input['attr'].type] != undefined) {
+                    //angegebene Datentypen zu Datenbank Typen kovertieren
+                    if (types[input['attr'].type] !== undefined) {
                         colum += types[input['attr'].type].sql;
                     } else {
-                        throw "Could not find type " + input['attr'].type + ". Resolve issue first!";
+                        throw "Es konnte kein Typ mit dem Namen " + input['attr'].type + " gefunden werden.";
                     }
                     if (input['attr'].required === 'true') {
                         colum += " NOT NULL"
@@ -44,14 +64,12 @@ for (let i = 2; i < process.argv.length; i++) {
                 });
             });
             // zu erstellende Colums dem SQLStatement anhängen
-            colums.forEach((colum, index) => {
+            colums.forEach(colum => {
                 sqlStatement += colum + ", ";
             })
             sqlStatement += " PRIMARY KEY (id) );";
-            console.log(sqlStatement);
             // console.log(sqlStatement);
             //Tabelle mit Namen und Colums in der Db anglegen
-            /*
             connection.query(sqlStatement, function (error, results) {
                 if (error) {
                     if (error.errno === 1050) {
@@ -63,7 +81,7 @@ for (let i = 2; i < process.argv.length; i++) {
                     console.log('Tabelle mit dem Namen: ' + objectName + ' wurde erfolgreich angelegt');
                 }
             });
-            */
+
         } else {
             console.log(error);
         }
